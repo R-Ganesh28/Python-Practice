474ba73932ff09fb0c3c4c0a42e523bfdf4ee899
Ganesh. R <130210233+Ganesh28122002@users.noreply.github.com>
2025-06-20 22:50:05 +0530

    Added Day 2 Array Problems

---
+++ Check if Array is Sorted or Not.py
@@ -0,0 +1,7 @@
+def ChkArr(arr1):
+    if (arr1 == sorted(arr1)):
+        print(True)
+    else:
+        print(False)
+arr1 = [2, 3, 4, 5, 6, 1]
+ChkArr(arr1)
---
+++ Second Largest Element in an Array.py
@@ -0,0 +1,5 @@
+def ScndLarge(arr):
+    arr.sort()
+    print(arr[-2])
+arr = [45, 10, 9, 20, 30, 5, 54]
+ScndLarge(arr)
