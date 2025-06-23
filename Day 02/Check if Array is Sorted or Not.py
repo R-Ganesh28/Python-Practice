ec05e80ed8c038e5db24a87be3f16357fac39d8f
Ganesh. R <130210233+Ganesh28122002@users.noreply.github.com>
2025-06-23 23:22:00 +0530

    flat structure

--- Day 02/Check if Array is Sorted or Not.py
+++
@@ -1,7 +0,0 @@
-def ChkArr(arr1):
-    if (arr1 == sorted(arr1)):
-        print(True)
-    else:
-        print(False)
-arr1 = [2, 3, 4, 5, 6, 1]
-ChkArr(arr1)
--- Day 02/Second Largest Element in an Array.py
+++
@@ -1,5 +0,0 @@
-def ScndLarge(arr):
-    arr.sort()
-    print(arr[-2])
-arr = [45, 10, 9, 20, 30, 5, 54]
-ScndLarge(arr)
