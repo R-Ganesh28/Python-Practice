3db88017312071e22c629502634ca6613feba5f2
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2025-07-16 23:04:58 +0530

    Prefix Sum & Binary Search

---
+++ LeetCode Before DSA Day 11/Binary_Search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+nums = [-1,0,3,5,9,12]
+target = 9
+sol = Solution()
+print(sol.search(nums, target))
